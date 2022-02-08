1f3b743fcc98e7fc6d7e6645b34c1fd9a1f12308
DhruvShah3664 <74092373+DhruvShah3664@users.noreply.github.com>
2022-02-08 19:33:17 +0530

    Add files via upload

---
+++ Guessthenumber.py
@@ -0,0 +1,21 @@
+print("Number Guessing Game")
+number = 6
+chances = 0
+print("Guess a number between 1-9")
+
+while chances < 5:
+    guess = int(input("Enter your guess:"))
+
+    if guess == number:
+        print("Congratulation You Won!!!")
+
+    elif guess < number:
+        print("Your guess was too low: Guess a number higher than", guess)
+
+    elif guess > number:
+        print("Your guess was too high: Guess a number lower than", guess)
+
+    chances +=1
+
+if not chances < 5:
+    print("Tou Lose!!! The number is", number)
